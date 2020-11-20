37643ca3746c19a61854d26a4405a27b172ca587
Alexey Khit <alexey.khit@gmail.com>
2020-11-20 23:58:19 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yandex Smart Home Fix
+
+[![hacs_badge](https://img.shields.io/badge/HACS-Custom-orange.svg)](https://github.com/custom-components/hacs)
---
+++ custom_components/yandex_smart_home_fix/__init__.py
@@ -0,0 +1,20 @@
+from aiohttp.frozenlist import FrozenList
+from aiohttp.web import middleware
+from aiohttp.web_response import Response
+from homeassistant.core import HomeAssistant
+
+
+async def async_setup(hass: HomeAssistant, hass_config: dict):
+    @middleware
+    async def fix_middleware(request, handler):
+        resp: Response = await handler(request)
+        resp._compression = False
+        return resp
+
+    hass.http.app._middlewares = FrozenList(
+        list(hass.http.app.middlewares) + [fix_middleware]
+    )
+    hass.http.app._middlewares_handlers = tuple(
+        hass.http.app._prepare_middleware()
+    )
+    return True
